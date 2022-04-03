5a52260bfa60248fd386ebf1e2dc16eb9561054b
Adalbert Pungu <18pw419@esisalama.org>
2022-04-03 10:42:41 +0200

    Déclarations de variables

---
+++ copierDonnerFr.vb
@@ -0,0 +1,9 @@
+Sub copierDonnerFr()
+
+    ' Déclaration des variables
+
+    Dim compteur1 As Long
+    Dim dernierLingeFeuil1 As Long, dernierLingeFeuil2 As Long
+    Dim mois As String
+    
+End Sub
