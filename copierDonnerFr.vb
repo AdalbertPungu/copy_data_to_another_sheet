4e125fa51e60840ede53a7405a945b860f066098
Adalbert Pungu <18pw419@esisalama.org>
2022-04-03 10:48:28 +0200

    Ajout d'un commentaire

--- copierDonnerFr.vb
+++ copierDonnerFr.vb
@@ -12,4 +12,8 @@ Sub copierDonnerFr()
     dernierLingeFeuil1 = Worksheets("feuil1").Range("A" & Rows.Count).End(xlUp).Row + 1
     dernierLingeFeuil2 = Worksheets("feuil2").Range("A" & Rows.Count).End(xlUp).Row + 1
     
+    ' Une boucle qui permet de parcourir la feuil1 qui
+    ' contient les données, il copie les données si le mois
+    ' de la cellule Date est egal à 03 donc Mars
+
 End Sub
