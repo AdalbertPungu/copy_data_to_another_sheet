7d29ca6bf6f78e5693a617a72ef223240573480d
Adalbert Pungu <18pw419@esisalama.org>
2022-04-03 10:45:23 +0200

    Copier les données d'une ligne entière

--- copierDonnerFr.vb
+++ copierDonnerFr.vb
@@ -5,5 +5,11 @@ Sub copierDonnerFr()
     Dim compteur1 As Long
     Dim dernierLingeFeuil1 As Long, dernierLingeFeuil2 As Long
     Dim mois As String
+
+    ' Dans la feuil1 on va copier les données de Rangé A du début
+    ' jusqu'à la fin.
+    
+    dernierLingeFeuil1 = Worksheets("feuil1").Range("A" & Rows.Count).End(xlUp).Row + 1
+    dernierLingeFeuil2 = Worksheets("feuil2").Range("A" & Rows.Count).End(xlUp).Row + 1
     
 End Sub
