a6da6b5e7445c7363eeefbc9802107aaf055b9e5
Adalbert Pungu <18pw419@esisalama.org>
2022-04-04 06:39:34 +0200

    on récupère la dernière ligne

--- copierDonnerUS.vb
+++ copierDonnerUS.vb
@@ -5,6 +5,14 @@ Sub copierDonnerUs()
     Dim compteur1 As Long
     Dim dernierLingeFeuil1 As Long, dernierLingeFeuil2 As Long
     Dim mois As String
+
+    ' Dans la feuil1 on va copier les données de Rangé A du début
+    ' jusqu'à la fin.
+    
+    dernierLingeFeuil1 = Worksheets("Sheet1").Range("A" & Rows.Count).End(xlUp).Row + 1
+    dernierLingeFeuil2 = Worksheets("Sheet2").Range("A" & Rows.Count).End(xlUp).Row + 1
+    
+    
 End Sub
 
 
