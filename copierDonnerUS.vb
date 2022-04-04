074959737f508234f24cde27fb499e9bad16ac26
Adalbert Pungu <18pw419@esisalama.org>
2022-04-04 06:35:13 +0200

    Faire une copie pour un classeur en anglais

---
+++ copierDonnerUS.vb
@@ -0,0 +1,12 @@
+Sub copierDonnerUs()
+
+    ' Déclaration des variables
+
+    Dim compteur1 As Long
+    Dim dernierLingeFeuil1 As Long, dernierLingeFeuil2 As Long
+    Dim mois As String    
+    
+End Sub
+
+
+
