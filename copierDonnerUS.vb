ade1383b43e9479a7cfe3998db26e4efd4f8b214
Adalbert Pungu <18pw419@esisalama.org>
2022-04-07 15:51:01 +0200

    Accès à la dernière ligne

--- copierDonnerUS.vb
+++ copierDonnerUS.vb
@@ -15,11 +15,17 @@ Sub copierDonnerUs()
     ' Une boucle qui permet de parcourir la feuil1 qui
     ' contient les données, il copie les données si le mois
     ' de Latest Hire Date est egal à 03 donc Mars
-    
+     
     For compteur1 = 2 To dernierLingeFeuil1
         
+        dernierLingeFeuil2 = Worksheets("Sheet2").Range("A" & Rows.Count).End(xlUp).Row + 1
+        mois = Mid(Worksheets("Sheet1").Range("L" & compteur1).Value, 4, 2)
+        
+        ' Si le mois egal à 03, on stocke dans la feuil2 les données
+        ' de la feuil1
+
     Next compteur1
-    
+
 End Sub
 
 
